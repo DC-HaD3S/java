7a45a75f89e34e6c6f6c12bb304edcd163368efc
Rohit Rajendra Zirmute <106946247+DC-HaD3S@users.noreply.github.com>
2022-06-28 10:27:38 +0530

    Add files via upload

---
+++ Practical/reverse2.java
@@ -0,0 +1,14 @@
+import java.util.scabber;
+class Array
+{
+public static void main(String args[])
+int []arr={1,2,3,4,5};//5 4 3 2 1
+System.out.println("Actual Array:");
+for(int i=0;i<arr.length;i++)//i<arr.length/i<5
+{
+System.out.println(arr[i]+"")};
+//Array in reverse order
+System.out.println("Array in reverse order");
+for(int i=arr.length-i;i>=0;i--)//4
+{
+System.out.println(arr[i]+"");}

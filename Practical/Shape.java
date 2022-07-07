a682731dae3d315bc21db63a6154b56108b64b63
Rohit Rajendra Zirmute <106946247+DC-HaD3S@users.noreply.github.com>
2022-07-07 10:43:46 +0530

    Add files via upload

---
+++ Practical/Shape.java
@@ -0,0 +1,25 @@
+class Circle {
+
+	float radius;
+
+	Circle() {
+
+		radius = 1.5f;
+	}
+
+	void show() {
+		System.out.println("Radius of the circle: " + radius);
+	}
+
+}
+
+
+public class Shape {
+	public static void main(String[] args) {
+
+		Circle r = new Circle();
+		r.show();
+
+	}
+
+}
